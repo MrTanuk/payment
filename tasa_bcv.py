ca2e4ffd8aedbe9456fcf3df9337a6d66635bfd8
Luis Ramírez <louismario.rr@gmail.com>
2024-06-23 22:26:57 -0400

    Adding tasa_bcv code

---
+++ tasa_bcv.py
@@ -0,0 +1,11 @@
+import lxml.html as web
+from urllib.request import urlopen
+
+url = "https://www.bcv.org.ve/"
+
+tree = web.parse(urlopen(url)).getroot()
+
+price_dolar = tree.xpath(".//div[@id='dolar']/div/div/div[@class='col-sm-6 col-xs-6 centrado']/strong/text()")
+price_dolar = price_dolar[0].replace(",", ".").strip()
+
+print(round(float(price_dolar), 2))
